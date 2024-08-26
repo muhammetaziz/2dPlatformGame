d330d9bf6d1a81b995590cb3a274ea358b949b9b
muhammetaziz <72401922+muhammetaziz@users.noreply.github.com>
2024-08-26 11:47:17 +0300

    26.08.2024

--- 2dGame/Assets/Scripts/Player/PlayerMove.cs
+++ 2dGame/Assets/Scripts/Player/PlayerMove.cs
@@ -1,19 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class PlayerMove : MonoBehaviour
 {
+    public int PlayerCoin;
+    public TextMeshProUGUI cointext;
 
     public Rigidbody2D rb;
     public CapsuleCollider2D col;
     private Renderer rd;
-
+    private bool grounded;
+    Explode explode;
     public float moveSpeed;
 
     private void Awake()
     {
+        explode = GetComponent<Explode>();
         rb = GetComponent<Rigidbody2D>();
         col = GetComponent<CapsuleCollider2D>();
         rd = GetComponent<Renderer>();
@@ -21,25 +27,55 @@ public class PlayerMove : MonoBehaviour
 
     private void Update()
     {
+        PlayerMovement();
         if (Input.GetKey(KeyCode.Space))
         {
 
             transform.localScale = new Vector2(2, 2);
             transform.position = new Vector2(0, 0);
-            rd.material.color=Color.black;
-            rb.
-        }
+            rd.material.color = Color.black;
 
-        if (Input.GetKey("w"))
-        {
-            rb.AddForce(Vector2.right * moveSpeed);
-            Debug.Log("w");
-        }
-
-        if (Input.GetKey("s"))
-        {
-            rb.AddForce(Vector2.left * moveSpeed);
-            Debug.Log("s");
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "engel")
+        {
+            PlayerCoin++;
+            Destroy(other.gameObject);
+            cointext.text = "Puan: " + PlayerCoin;
+        }
+        if (other.gameObject.tag == "graund")
+        {
+            grounded = true;
+        }
+        else
+        {
+            grounded = false;
+        }
+    }
+    private void PlayerMovement()
+    {
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(Vector2.right * moveSpeed);
+
+        }
+        else if (Input.GetKey("a"))
+        {
+            rb.AddForce(Vector2.left * moveSpeed);
+        }
+        else if (Input.GetKey("w"))
+        {
+            if (grounded == false)
+            {
+                rb.AddForce(Vector2.up * moveSpeed);
+            }
+        }
+
+
+
+    }
+
 }
